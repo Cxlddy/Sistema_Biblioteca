b1445bd423b12596e3b69dbd17491c435be3c7f4
Cxlddy <rcds.ciampi@gmail.com>
2026-03-16 16:16:19 -0300

    solving bugs with the database

--- database/database.py
+++ database/database.py
@@ -3,15 +3,16 @@ import os
 
 CAMINHO_DB = os.path.join(os.path.dirname(__file__), "database.json")
 
-
 def carregar_dados():
-    try:
-        with open(CAMINHO_DB, "r", encoding="utf-8") as f:
-            return json.load(f)
-    except:
+
+    if not os.path.exists(CAMINHO_DB):
         return {"livros": [], "usuarios": []}
+
+    with open(CAMINHO_DB, "r", encoding="utf-8") as f:
+        return json.load(f)
 
 
 def salvar_dados(dados):
+
     with open(CAMINHO_DB, "w", encoding="utf-8") as f:
         json.dump(dados, f, indent=4, ensure_ascii=False)
